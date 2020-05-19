eea785d0ea497e2ff06ed68cac97d214d6967ba3
Marco Endres <marco.endres@clavisit.com>
2020-05-19 11:19:10 +0200

    remove not necessary handlebars function

--- js/handlebarsHelper.js
+++ js/handlebarsHelper.js
@@ -8,10 +8,6 @@ Handlebars.registerHelper('ifCond', function (v1, v2, options) {
   return options.inverse(this)
 })
 
-Handlebars.registerHelper('ternary', function (test, yes, no) {
-  return test ? yes : no
-})
-
 Handlebars.registerHelper('buttonIcon', function (currentHand) {
   let currGame = controller.getCurrentGame()
   return currGame && currGame.playerHand === currentHand ? `icon-${currGame.result}` : ''
